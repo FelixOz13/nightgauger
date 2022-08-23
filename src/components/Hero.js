5f1f3de373cd6d30a5603739c171c23400882083
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2022-08-23 01:28:40 -0500

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,44 @@
+
+
+function Hero() {
+  return (
+    <div className='hero'>
+      <img className='heropic' src="assets/coverpic.png" alt="" />
+      <img className='heropic' src="assets/gasflow19.png" alt="" />
+      <h1 className='herotext'>Automatic Lift for
+Deliquefying
+Flowing Oil & Gas
+Wells using
+        Foaming Sticks </h1>
+      <h2>Solid foam sticks, applied once per day, are designed to increase natural
+gas production by an average of 30-35%, by reducing the hydrostatic
+pressure on layer, achieved by the transformation in foam of the
+deposit water accumulated in well.
+Automatic Launchers, combined with Well Limiters, can increase
+        production 40% -50% and higher.</h2>
+      <img className='heropic' src="assets/gasflow.png" alt="" />
+      
+      <img className='heropic' src="assets/gasflow2.png" alt="" />
+      <h3>Unloading liquids from aging gas wells is a problem that every drilled well will eventually have to solve. There are many solutions marketed today but the US EPA did a study in 2010 and found Foam (Soap) Sticks as the most economical. All Technologies strive to do the same thing, Deliquefy the well. Since the the well is nearing the end of it's life, it only makes sense to minimalize the Capital Investment </h3>
+      
+      <img className='heropic' src="assets/gasflow3.png" alt="" />
+      <img className='heropic' src="assets/gasflow4.png" alt="" />
+      <img className='heropic' src="assets/gasflow5.png" alt="" />
+       <img className='heropic' src="assets/gasflow6.png" alt="" />
+      <img className='heropic' src="assets/gasflow7.png" alt="" />
+      <img className='heropic' src="assets/gasflow8.png" alt="" />
+      <img className='heropic' src="assets/gasflow9.png" alt="" />
+      <img className='heropic' src="assets/gasflow10.png" alt="" />
+      <img className='heropic' src="assets/gasflow11.png" alt="" />
+      <img className='heropic' src="assets/gasflow12.png" alt="" />
+      <img className='heropic' src="assets/gasflow13.png" alt="" />
+      <img className='heropic' src="assets/gasflow14.png" alt="" />
+    
+      <img className='heropic' src="assets/gasflow16.png" alt="" />
+      <img className='heropic' src="assets/gasflow18.png" alt="" />
+      <img className='heropic' src="assets/gasflow17.png" alt="" />
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './Navbar/NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+       
+        <NavLink to="/">
+           <img className="navlogo" src="assets/logo.png" width=" 80px" alt="" />
+          <h1 className="title">Night Gauger</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+          <NavLink to="/feedback" activeStyle>
+            Feedback
+          </NavLink>
+        </NavMenu>
+        
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar
